8d65fd47516c69dfa9898d4e0cbd57e1654ffae8
Andrei Shakh <andrey_overdose@mail.ru>
2023-04-21 15:03:08 +0300

    first

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return 'signup';
+  }
+
+  signin() {
+    return 'signin';
+  }
+}
